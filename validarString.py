279532ef6de01b3a684e5528ea50d9fab42a71cc
Wellington Fonseca <64553168+W-Fonseca@users.noreply.github.com>
2021-09-28 20:53:40 -0300

    Create validarString.py

---
+++ validarString.py
@@ -0,0 +1,20 @@
+def validar(texto):
+    import re
+    lista_Componentes = []
+    data = re.findall("[\d]{1,2}(?:.|/|\|)[\d]{1,2}(?:.|/|\|)[\d]{2,4}", texto)
+    print(data)
+    print(texto)
+    quantidade = texto.count("[")
+
+    for c in range(0, quantidade):
+        local1 = texto.index("[")
+        local2 = texto.index("]")
+        item = texto[local1:local2+1].replace("[","").replace("]","")
+        print(item)
+        lista_Componentes += [item]
+        print(lista_Componentes)
+        texto = texto.replace(texto[local1:local2+1],"")
+
+
+
+validar("[RFQ]_[COUPA]_[28.09.2021]_[EFA]_1")
